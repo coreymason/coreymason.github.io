eaff891b1c6e7dbdcf1a3dd8917c7b1456affe84
Corey Mason <corey.mason98@googlemail.com>
2015-03-17 19:06:09 -0700

    Fix Header Centering and Height
    Also creates a canvas and sets up a loop for animation.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,76 @@
 $(document).ready(function() {
-    cycle();
+    initCycle(false);
+    initBackground();
+    addListeners();
+    initCycle(true);
+    loop();
 });
 
-var divs = $('h2[id^="cycle"]').hide();
-var i = 0;
+var width, height, header, canvas, intro;
+var cycleDivs, cycleCount;
 
-function cycle() {
-  divs.eq(i).fadeIn(400)
-              .delay(1000)
-              .fadeOut(400, cycle);
+function initCycle(ready) {
+  if(ready) {
+    cycleDivs.eq(cycleCount).hide().css('visibility','visible');
+  } else {
+    cycleCount = 0
+    cycleDivs = $('h2[id^="cycle"]').hide();
+    cycleDivs.eq(cycleCount).css('visibility','hidden').show();
+  }
+}
 
-    i = ++i % divs.length;
+//still need to address the change in header div heightwhen text takes more than one line
+//the canvas doesn't update and ideally this header div shouldn't have to change size at all
+function initBackground() {
+  width = window.innerWidth;
+  height = window.innerHeight;
+
+  header = document.getElementById('header');
+  header.style.minHeight = height+'px';
+
+  canvas = document.getElementById('background');
+  canvas.width = header.offsetWidth;
+  canvas.height = header.offsetHeight;
+
+  greeting = document.getElementById('greeting');
+  offset = height/2 - (greeting.offsetHeight+10)/2;
+  if(offset > 0) {
+    greeting.style.marginTop = offset+'px';
+  } else {
+    greeting.style.marginTop = 0;
+  }
+}
+
+function addListeners() {
+    window.addEventListener('scroll', scrollCheck);
+    window.addEventListener('resize', resize);
+}
+
+function scrollCheck() {
+    
+}
+
+function resize() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+
+    header.style.minHeight = height+'px';
+
+    canvas.width = header.offsetWidth;
+    canvas.height = header.offsetHeight;
+
+    offset = height/2 - (greeting.offsetHeight+10)/2;
+    if(offset > 0) {
+      greeting.style.marginTop = offset+'px';
+    } else {
+      greeting.style.marginTop = 0;
+    }
+}
+
+function loop() {
+  //TODO: Animation
+
+  //cycle and repeat
+  cycleDivs.eq(cycleCount).fadeIn(400).delay(1000).fadeOut(400, loop);
+  cycleCount = ++cycleCount % cycleDivs.length;
 }
