7e25465008163bbf9c07f031dbf945c75b20ac97
Corey Mason <corey.mason98@googlemail.com>
2015-07-01 18:18:38 -0700

    Started Tabs

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
     addListeners();
     initCycle(true);
     initParticles();
+    initTabs();
+    initJump();
     loop();
 });
 
@@ -83,5 +85,83 @@ function initParticles() {
   /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
   particlesJS.load('particles-js', 'js/particles.json', function() {
     console.log('callback - particles.js config loaded');
+  });
+}
+
+//initializes tabs -- modified but original credit to http://codyhouse.co/
+function initTabs() {
+  var tabs = $('.tabs');
+
+	tabs.each(function(){
+		var tab = $(this),
+			tabItems = tab.find('ul.tabs-navigation'),
+			tabContentWrapper = tab.children('ul.tabs-content'),
+			tabNavigation = tab.find('nav');
+
+		tabItems.on('click', 'a', function(event){
+			event.preventDefault();
+			var selectedItem = $(this);
+			if( !selectedItem.hasClass('selected') ) {
+				var selectedTab = selectedItem.data('content'),
+					selectedContent = tabContentWrapper.find('li[data-content="'+selectedTab+'"]'),
+					selectedContentHeight = selectedContent.innerHeight();
+
+				tabItems.find('a.selected').removeClass('selected');
+				selectedItem.addClass('selected');
+				selectedContent.addClass('selected').siblings('li').removeClass('selected');
+				//animate tabContentWrapper height when content changes
+				tabContentWrapper.animate({
+					'height': selectedContentHeight
+				}, 200);
+			}
+		});
+
+		//hide the .tabs::after element when tabbed navigation has scrolled to the end (mobile version)
+		checkScrolling(tabNavigation);
+		tabNavigation.on('scroll', function(){
+			checkScrolling($(this));
+		});
+	});
+
+	$(window).on('resize', function(){
+		tabs.each(function(){
+			var tab = $(this);
+			checkScrolling(tab.find('nav'));
+			tab.find('.tabs-content').css('height', 'auto');
+		});
+	});
+
+	function checkScrolling(tabs){
+		var totalTabWidth = parseInt(tabs.children('.tabs-navigation').width()),
+		 	tabsViewport = parseInt(tabs.width());
+		if( tabs.scrollLeft() >= totalTabWidth - tabsViewport) {
+			tabs.parent('.tabs').addClass('is-ended');
+		} else {
+			tabs.parent('.tabs').removeClass('is-ended');
+		}
+	}
+}
+
+//initializes links to tabs
+function initJump() {
+  var jumpItems = $('.intro').find('.jump');
+  var tabItems = $('.tabs').find('ul.tabs-navigation');
+  var tabContentWrapper = $('.tabs').children('ul.tabs-content');
+
+  jumpItems.on('click', 'a', function(event){
+    event.preventDefault();
+    var selectedJump = $(this);
+    var selectedTab = selectedJump.data('content');
+    var selectedItem = tabItems.find('a[data-content="'+selectedTab+'"]');
+    var selectedContent = tabContentWrapper.find('li[data-content="'+selectedTab+'"]');
+    var selectedContentHeight = selectedContent.innerHeight();
+
+    tabItems.find('a.selected').removeClass('selected');
+    selectedItem.addClass('selected');
+    selectedContent.addClass('selected').siblings('li').removeClass('selected');
+    //animate tabContentWrapper height when content changes
+    tabContentWrapper.animate({
+      'height': selectedContentHeight
+    }, 200);
   });
 }
