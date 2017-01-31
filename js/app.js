0105cf3eae8a07cad84e518f8579864d6eff1e9a
Corey Mason <corey.mason98@googlemail.com>
2017-01-30 22:41:02 -0800

    Particles

--- js/app.js
+++ js/app.js
@@ -2,4 +2,7 @@ $("#header").headroom(),
 $(window).scroll(function() {
     var o = $(window).scrollTop();
     o >= 5 ? $("header").removeClass("nav-root") : 6 >= o && $("header").addClass("nav-root")
+}),
+particlesJS.load('particles-js', 'js/particles.json', function() {
+  console.log('callback - particles.js config loaded');
 });
