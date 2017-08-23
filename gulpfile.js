eedf09ca1b55e3f9057346a36b73ba2cb6cb84b5
Corey Mason <corey.mason98@googlemail.com>
2017-08-23 00:29:11 -0700

    Source Maps

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,16 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
   return gulp.src('./assets/sass/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./assets/css/dist'));
 });
+
 
 gulp.task('sass:watch', function () {
   gulp.watch('./assets/sass/**/*.scss', ['sass']);
