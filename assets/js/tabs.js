823a4d9d556c92754eb76e8f208fde2fa3a045ce
Corey Mason <corey.mason98@googlemail.com>
2017-08-24 10:23:01 -0700

    Tab Button Navigation

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -5,6 +5,7 @@
         var base = this;
         base.$el = $(el);
         base.$nav = base.$el.find("#nav");
+        base.$buttons = base.$el.find("#nav-buttons");
 
         base.init = function() {
 
@@ -18,21 +19,40 @@
                 "display": "none"
             });
 
+            base.$buttons.delegate("li > a.button", "click", function() {
+              var curList = base.$el.find("li.active > a").attr("href").substring(1);
+              var listID = $(this).attr("href").substring(1);
+              var $newList = base.$el.find("#"+listID+"-tab");
+
+              $("html, body").animate({ scrollTop: $("#header").offset().top }, "slow");
+
+              base.$el.find("#"+curList).fadeOut(base.options.speed, function() {
+                  // Fade in new list on callback
+                  base.$el.find("#"+listID).fadeIn(base.options.speed);
+
+                  // Remove highlighting - Add to just-clicked tab
+                  base.$el.find("#nav li").removeClass("active");
+                  $newList.parent().addClass("active");
+              });
+
+              return false;
+            });
+
             base.$nav.delegate("li > a.tab", "click", function() {
 
                 // Figure out current list via CSS class
-                var curList = base.$el.find("li.active > a").attr("href").substring(1),
+                var curList = base.$el.find("li.active > a").attr("href").substring(1);
 
                 // List moving to
-                    $newList = $(this),
+                var $newList = $(this);
 
                 // Figure out ID of new list
-                    listID = $newList.attr("href").substring(1),
+                var listID = $newList.attr("href").substring(1);
 
                 // Set outer wrapper height to (static) height of current inner list
-                    $allListWrap = base.$el.find(".list-wrap"),
-                    curListHeight = $allListWrap.height();
-                $allListWrap.height(curListHeight);
+                // $allListWrap = base.$el.find(".list-wrap"),
+                // curListHeight = $allListWrap.height();
+                // $allListWrap.height(curListHeight);
 
                 if ((listID != curList) && ( base.$el.find(":animated").length == 0)) {
 
@@ -43,10 +63,10 @@
                         base.$el.find("#"+listID).fadeIn(base.options.speed);
 
                         // Adjust outer wrapper to fit new list snuggly
-                        var newHeight = base.$el.find("#"+listID).height();
-                        $allListWrap.animate({
-                            height: newHeight
-                        });
+                        // var newHeight = base.$el.find("#"+listID).height();
+                        // $allListWrap.animate({
+                        //     height: newHeight
+                        // });
 
                         // Remove highlighting - Add to just-clicked tab
                         base.$el.find("#nav li").removeClass("active");
